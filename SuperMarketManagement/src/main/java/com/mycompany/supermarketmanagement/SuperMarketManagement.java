fd430915677fd463eae1904363c009d76309a668
pravinbiswas2000 <143477281+pravinbiswas2000@users.noreply.github.com>
2023-10-08 13:07:37 +0530

    Add files via upload

---
+++ SuperMarketManagement/src/main/java/com/mycompany/supermarketmanagement/SuperMarketManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermarketmanagement;
+
+/**
+ *
+ * @author pravi
+ */
+public class SuperMarketManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
